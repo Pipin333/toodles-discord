545d074b64ea396a0efb8ba55f2e47496a941987
Pipin333 <144046428+Pipin333@users.noreply.github.com>
2024-09-12 00:24:19 -0300

    Update suzuranMusic.py

--- suzuranMusic.py
+++ suzuranMusic.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from pytube import YouTube
+import yt_dlp as youtube_dl  # Usa yt-dlp en lugar de pytube
 import asyncio
 
 # Music-related functions
@@ -47,27 +47,25 @@ class Music(commands.Cog):
             await ctx.send("Ya estoy tocando música.")
             return
 
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'quiet': True,
+            'noplaylist': True,  # Para evitar descargar listas de reproducción
+        }
+
         try:
-            yt = YouTube(url)
-            audio_stream = yt.streams.filter(only_audio=True).first()
-            audio_url = audio_stream.url
-            source = discord.FFmpegPCMAudio(audio_url)
-            voice_client.play(source)
-            await ctx.send(f"Reproduciendo: **{yt.title}**")
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                info = ydl.extract_info(url, download=False)
+                if 'formats' in info:
+                    url2 = info['formats'][0]['url']
+                    source = discord.FFmpegPCMAudio(url2, **{'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5'})
+                    voice_client.play(source)
+                    await ctx.send(f"Reproduciendo: **{info['title']}**")
+                else:
+                    await ctx.send("No se pudo obtener el audio de la URL proporcionada.")
         except Exception as e:
             await ctx.send(f"Hubo un error al intentar reproducir el audio: {e}")
             print(f"Error al intentar reproducir el audio: {e}")
-        
-    @commands.command()
-    async def leave(self, ctx):
-        """Bot leaves the voice channel"""
-        voice_client = ctx.voice_client
-        if voice_client:
-            await voice_client.disconnect()
-            await ctx.send("noh vimo xao")
-        else:
-            await ctx.send("No estoy en un canal de voz para desconectarme.")
-
 # Setup the cog
 async def setup(bot):
     await bot.add_cog(Music(bot))
