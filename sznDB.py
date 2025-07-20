770c8a613a5d8e9032027d15df1118a766100c9c
Pipin333 <f.riquelmesalvo@uandresbello.edu>
2025-07-20 00:15:45 -0400

    v6 Beta 1

--- sznDB.py
+++ sznDB.py
@@ -9,6 +9,32 @@ from cryptography.fernet import Fernet
 from database import Session, AppConfig
 import os
 
+FERNET_KEY = os.getenv("FERNET_KEY")
+fernet = Fernet(FERNET_KEY) if FERNET_KEY else None
+
+def save_config(key: str, value: str):
+        if fernet:
+            value = fernet.encrypt(value.encode()).decode()
+        with Session.begin() as session:
+            existing = session.query(AppConfig).filter_by(key=key).first()
+            if existing:
+                existing.value = value
+            else:
+                session.add(AppConfig(key=key, value=value))
+
+def load_config(key: str) -> str | None:
+        with Session.begin() as session:
+            entry = session.query(AppConfig).filter_by(key=key).first()
+            if entry:
+                if fernet:
+                    try:
+                        return fernet.decrypt(entry.value.encode()).decode()
+                    except Exception as e:
+                        print(f"❌ Error al desencriptar valor de {key}: {e}")
+                        return None
+                return entry.value
+        return None
+
 Base = declarative_base()
 
 class UserLike(Base):
@@ -216,31 +242,6 @@ class MusicDB(commands.Cog):
         embed.set_footer(text="Basado en estadísticas del bot")
         return embed
     
-    FERNET_KEY = os.getenv("FERNET_KEY")
-    fernet = Fernet(FERNET_KEY) if FERNET_KEY else None
-
-    def save_config(key: str, value: str):
-        if fernet:
-            value = fernet.encrypt(value.encode()).decode()
-        with Session.begin() as session:
-            existing = session.query(AppConfig).filter_by(key=key).first()
-            if existing:
-                existing.value = value
-            else:
-                session.add(AppConfig(key=key, value=value))
-
-    def load_config(key: str) -> str | None:
-        with Session.begin() as session:
-            entry = session.query(AppConfig).filter_by(key=key).first()
-            if entry:
-                if fernet:
-                    try:
-                        return fernet.decrypt(entry.value.encode()).decode()
-                    except Exception as e:
-                        print(f"❌ Error al desencriptar valor de {key}: {e}")
-                        return None
-                return entry.value
-        return None
 async def setup(bot):
     musicdb = MusicDB(bot)
     await bot.add_cog(musicdb)
